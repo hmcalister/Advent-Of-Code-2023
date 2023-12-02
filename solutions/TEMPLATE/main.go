0c8595d5dd4dcb29405a9c32fc6b11865d102614
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-03 04:42:56 +1300

    Add a template directory for future days

---
+++ solutions/TEMPLATE/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"hmcalister/aocTemplate/part01"
+	"log"
+	"os"
+)
+
+const INPUT_FILE_PATH = "puzzleInput"
+
+func main() {
+	file, err := os.Open(INPUT_FILE_PATH)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	result, err := part01.ProcessInput(fileScanner)
+	if err != nil {
+		log.Fatalf("error processing file input: %v", err)
+	}
+
+	log.Printf("result: %v", result)
+}
