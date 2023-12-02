7f19974de119fcba081b0c220316bbc65fd26ec1
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-03 04:26:24 +1300

    Update part02.go to use parallel processing (for fun)

--- solutions/02/part02/part02.go
+++ solutions/02/part02/part02.go
@@ -2,10 +2,14 @@ package part02
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type trialData struct {
@@ -37,7 +41,7 @@ func makeTrialData(trialString string) (*trialData, error) {
 		} else if strings.HasSuffix(observation, " blue") {
 			td.blueCube = observationNumber
 		} else {
-			return nil, fmt.Errorf("error in observation %v", observationIndex)
+			return nil, fmt.Errorf("error in observation %v: %v", observationIndex, observation)
 		}
 	}
 
@@ -72,7 +76,7 @@ func processLine(line string) (int, error) {
 	for trialIndex, trial := range trials {
 		td, err := makeTrialData(trial)
 		if err != nil {
-			return -1, errors.Join(fmt.Errorf("error in trial %v", trialIndex), err)
+			return -1, errors.Join(fmt.Errorf("error in trial %v: %v", trialIndex, trial), err)
 		}
 		gd.addTrialInformation(td)
 	}
@@ -84,18 +88,58 @@ func processLine(line string) (int, error) {
 // satisfy the conditions of the number of red, green, and blue cubes in each
 // trial (separated by semicolons).
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
+	// Context for the error group that will process each line in parallel
+	ctx := context.Background()
+	errGroup, _ := errgroup.WithContext(ctx)
+
+	// Channel for results of each game / line
+	resultsChan := make(chan int)
+
+	// Sum of all game powers, computed in goroutine below
 	result := 0
 
-	for fileScanner.Scan() {
-		line := fileScanner.Text()
-		gameID, _ := strconv.Atoi(line[5:strings.IndexByte(line, ':')])
-		gamePower, err := processLine(line)
-		if err != nil {
-			return -1, errors.Join(fmt.Errorf("failed to process Game %v", gameID), err)
-		}
+	// WaitGroup to tell when summation has finished
+	var resultSumWaitGroup sync.WaitGroup
 
-		result += gamePower
+	// Start the summation goroutine, use resultSumWaitGroup to tell when it is done
+	resultSumWaitGroup.Add(1)
+	go func() {
+		for receivedResult := range resultsChan {
+			result += receivedResult
+		}
+		resultSumWaitGroup.Done()
+	}()
+
+	// For each line in the file
+	for fileScanner.Scan() {
+		// Grab the text of this line
+		line := fileScanner.Text()
+
+		// Grab the game ID, we can assume the file is nicely formatted such that this doesn't error
+		gameID, _ := strconv.Atoi(line[5:strings.IndexByte(line, ':')])
+
+		// Start a goroutine in our errgroup to process this line
+		errGroup.Go(func() error {
+			// Variable shadowing to copy variables to within this closure
+			gameID, line := gameID, line
+			gamePower, err := processLine(line)
+			if err != nil {
+				return errors.Join(fmt.Errorf("failed to process Game %v", gameID), err)
+			}
+
+			resultsChan <- gamePower
+			return nil
+		})
 	}
+
+	// Wait for the parallel line processing to finish
+	if err := errGroup.Wait(); err != nil {
+		return -1, err
+	}
+	// Close the results channel, now result summation goroutine knows there is no more data
+	close(resultsChan)
+	// Finally, wait for results summation goroutine to finish (in case there was a bottleneck here)
+	resultSumWaitGroup.Wait()
 
 	return result, nil
 }
