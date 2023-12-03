5af2c0f8036ada862827648778a098586ffe9f6a
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-03 16:20:49 +1300

    Update main with zerolog

--- solutions/02/main.go
+++ solutions/02/main.go
@@ -3,24 +3,34 @@ package main
 import (
 	"bufio"
 	"hmcalister/aoc02/part02"
-	"log"
 	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 const INPUT_FILE_PATH = "puzzleInput"
 
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+}
+
 func main() {
 	file, err := os.Open(INPUT_FILE_PATH)
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		log.Panic().Msgf("error opening file: %v", err)
 	}
 	defer file.Close()
 
 	fileScanner := bufio.NewScanner(file)
 	result, err := part02.ProcessInput(fileScanner)
 	if err != nil {
-		log.Fatalf("error processing file input: %v", err)
+		log.Panic().Msgf("error processing file input: %v", err)
 	}
 
-	log.Printf("result: %v", result)
+	log.Info().
+		Int("Result", result).
+		Send()
 }
