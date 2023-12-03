cbd8520b29a1d1ed2d30abfae357e0e542252fb8
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-03 16:22:08 +1300

    Update template project to use zerolog

--- solutions/TEMPLATE/main.go
+++ solutions/TEMPLATE/main.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"bufio"
-	"hmcalister/aocTemplate/part01"
-	"log"
+	"hmcalister/aoc02/part01"
 	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 const INPUT_FILE_PATH = "puzzleInput"
 
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+}
+
 func main() {
 	file, err := os.Open(INPUT_FILE_PATH)
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		log.Panic().Msgf("error opening file: %v", err)
 	}
 	defer file.Close()
 
 	fileScanner := bufio.NewScanner(file)
 	result, err := part01.ProcessInput(fileScanner)
 	if err != nil {
-		log.Fatalf("error processing file input: %v", err)
+		log.Panic().Msgf("error processing file input: %v", err)
 	}
 
-	log.Printf("result: %v", result)
+	log.Info().
+		Int("Result", result).
+		Send()
 }
