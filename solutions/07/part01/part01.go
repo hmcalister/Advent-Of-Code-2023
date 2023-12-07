a7a2a2e6ea1586f9ac99a21a25acb2f56334081c
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-07 22:21:56 +1300

    Update part01 with implementation

---
+++ solutions/07/part01/part01.go
@@ -0,0 +1,38 @@
+package part01
+
+import (
+	"bufio"
+	"hmcalister/aoc07/lib"
+	"sort"
+
+	"github.com/rs/zerolog/log"
+)
+
+func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
+	allHands := make([]lib.HandData, 0)
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		allHands = append(allHands, lib.ParseLineToHandData(line))
+	}
+
+	log.Debug().
+		Int("NumHandsFound", len(allHands)).
+		Send()
+
+	sort.Slice(allHands, func(i, j int) bool {
+		return allHands[i].HandStrength < allHands[j].HandStrength
+	})
+
+	result := 0
+	for handPosition, hand := range allHands {
+		handWinnings := hand.BidAmount * (handPosition + 1)
+		log.Debug().
+			Int("HandPosition", handPosition).
+			Int("HandWinnings", handWinnings).
+			Interface("Hand", hand).
+			Send()
+		result += handWinnings
+	}
+
+	return result, nil
+}
