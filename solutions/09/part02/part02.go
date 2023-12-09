e2495f02b3dde5018bcbfff9e08c88add7c77487
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-09 18:45:46 +1300

    Add part02 implementation

---
+++ solutions/09/part02/part02.go
@@ -0,0 +1,141 @@
+package part02
+
+import (
+	"bufio"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+type historyData struct {
+	HistoricValues []int
+}
+
+func parseLineToHistoryData(line string) historyData {
+	fields := strings.Fields(line)
+	history := historyData{
+		HistoricValues: make([]int, len(fields)),
+	}
+
+	var err error
+	for i, f := range fields {
+		history.HistoricValues[i], err = strconv.Atoi(f)
+		if err != nil {
+			log.Panic().Msgf("failed to parse history value %v at index %v in line %v", f, i, line)
+		}
+	}
+
+	return history
+}
+
+func (history historyData) findNextValueInSequence() int {
+	var currentSequence []int
+	var nextSequence []int
+	var nextSequenceAllZero bool
+	lastItemInSequences := make([]int, 0)
+
+	currentSequence = history.HistoricValues
+
+	// While we do not have a zero sequence
+	nextSequenceAllZero = false
+	for !nextSequenceAllZero {
+		nextSequence = make([]int, len(currentSequence)-1)
+		nextSequenceAllZero = true
+		for i := 0; i < len(currentSequence)-1; i += 1 {
+			nextSequence[i] = currentSequence[i+1] - currentSequence[i]
+			if nextSequence[i] != 0 {
+				nextSequenceAllZero = false
+			}
+		}
+		log.Trace().
+			Bool("NextSequenceAllZeros", nextSequenceAllZero).
+			Interface("NextSequence", nextSequence).
+			Send()
+
+		lastItemInSequences = append(lastItemInSequences, currentSequence[len(currentSequence)-1])
+		currentSequence = nextSequence
+	}
+
+	log.Trace().
+		Interface("LastItemInSequences", lastItemInSequences).
+		Send()
+
+	nextTerm := 0
+	slices.Reverse(lastItemInSequences)
+	for _, s := range lastItemInSequences {
+		log.Trace().
+			Int("CurrentNextTerm", nextTerm).
+			Int("NextLastItem", s).
+			Int("NextTerm", nextTerm+s).
+			Send()
+		nextTerm += s
+	}
+
+	return nextTerm
+}
+
+func (history historyData) findPreviousValueInSequence() int {
+	var currentSequence []int
+	var nextSequence []int
+	var nextSequenceAllZero bool
+	firstItemInSequences := make([]int, 0)
+
+	currentSequence = make([]int, len(history.HistoricValues))
+	copy(currentSequence, history.HistoricValues)
+	// slices.Reverse(currentSequence)
+
+	// While we do not have a zero sequence
+	nextSequenceAllZero = false
+	for !nextSequenceAllZero {
+		nextSequence = make([]int, len(currentSequence)-1)
+		nextSequenceAllZero = true
+		for i := 0; i < len(currentSequence)-1; i += 1 {
+			nextSequence[i] = currentSequence[i+1] - currentSequence[i]
+			if nextSequence[i] != 0 {
+				nextSequenceAllZero = false
+			}
+		}
+		log.Trace().
+			Bool("NextSequenceAllZeros", nextSequenceAllZero).
+			Interface("NextSequence", nextSequence).
+			Send()
+
+		firstItemInSequences = append(firstItemInSequences, currentSequence[0])
+		currentSequence = nextSequence
+	}
+
+	cumulativeTerm := 0
+	slices.Reverse(firstItemInSequences)
+	log.Trace().
+		Interface("FirstItemInSequences", firstItemInSequences).
+		Send()
+	for _, s := range firstItemInSequences {
+		log.Trace().
+			Int("CurrentCumulativeTerm", cumulativeTerm).
+			Int("NextFirstItem", s).
+			Int("NextTerm", s-cumulativeTerm).
+			Send()
+		cumulativeTerm = s - cumulativeTerm
+	}
+
+	return cumulativeTerm
+}
+
+func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
+	result := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		history := parseLineToHistoryData(line)
+		log.Debug().
+			Str("ParsedLine", line).
+			Interface("ParsedHistory", history).
+			Send()
+
+		result += history.findPreviousValueInSequence()
+	}
+
+	return result, nil
+}
