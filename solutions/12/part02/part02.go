86e3c6cc1a4fd50b221e761cee3891cfc5c54811
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-12 22:54:51 +1300

    Update part02 with parser logic

--- solutions/12/part02/part02.go
+++ solutions/12/part02/part02.go
@@ -3,15 +3,53 @@ package part02
 import (
 	"bufio"
 	"hmcalister/aoc12/lib"
+	"strconv"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 )
+
+func parseLineToSpringRowData(line string) lib.SpringRowData {
+	fields := strings.Fields(line)
+	log.Trace().
+		Str("ParsedLine", line).
+		Interface("Fields", fields).
+		Msg("Parsing Line To Data")
+
+	contiguousDamagedGroupDataStrs := strings.Split(fields[1], ",")
+	contiguousDamagedGroupData := make([]int, len(contiguousDamagedGroupDataStrs))
+	for i, str := range contiguousDamagedGroupDataStrs {
+		parsedInt, err := strconv.Atoi(str)
+		if err != nil {
+			log.Fatal().Msgf("failed to parsed contiguousDamagedGroup string %v to integer on line %v", str, line)
+		}
+		log.Trace().
+			Str("ContiguousGroupString", str).
+			Int("ParsedInt", parsedInt).
+			Send()
+		contiguousDamagedGroupData[i] = parsedInt
+	}
+
+	fiveTimesContiguousDamagedGroupData := make([]int, 5*len(contiguousDamagedGroupData))
+	for index := range fiveTimesContiguousDamagedGroupData {
+		fiveTimesContiguousDamagedGroupData[index] = contiguousDamagedGroupData[index%len(contiguousDamagedGroupData)]
+	}
+
+	rowLine := strings.Join([]string{fields[0], fields[0], fields[0], fields[0], fields[0]}, "?")
+	rowLine = strings.Trim(rowLine, string(lib.OPERATIONAL_SPRING_RUNE))
+
+	return lib.SpringRowData{
+		RowLine:                    rowLine,
+		ContiguousDamagedGroupData: fiveTimesContiguousDamagedGroupData,
+	}
+}
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
 	result := 0
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		row := lib.ParseLineToSpringRowData(line)
+
+		row := parseLineToSpringRowData(line)
 		rowArrangements := lib.CalculatePossibleArrangements(row.RowLine, row.ContiguousDamagedGroupData)
 		result += rowArrangements
 
