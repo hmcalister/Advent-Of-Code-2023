53ae5a136fa9f146f29819ae92d677c95acb5965
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-12 22:47:58 +1300

    Add part02 skeleton

--- solutions/12/part02/part02.go
+++ solutions/12/part02/part02.go
@@ -1,7 +1,26 @@
 package part02
 
-import "bufio"
+import (
+	"bufio"
+	"hmcalister/aoc12/lib"
+
+	"github.com/rs/zerolog/log"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	return 0, nil
+	result := 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		row := lib.ParseLineToSpringRowData(line)
+		rowArrangements := lib.CalculatePossibleArrangements(row.RowLine, row.ContiguousDamagedGroupData)
+		result += rowArrangements
+
+		log.Debug().
+			Str("Line", line).
+			Interface("SpringRow", row).
+			Int("PossibleArrangements", rowArrangements).
+			Int("CumulativeResult", result).
+			Send()
+	}
+	return result, nil
 }
