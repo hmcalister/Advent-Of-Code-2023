989c07da36d165842735a529a7522bcfcf160457
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-13 22:15:26 +1300

    Add part02 implementation

--- solutions/13/part02/part02.go
+++ solutions/13/part02/part02.go
@@ -1,7 +1,23 @@
 package part02
 
-import "bufio"
+import (
+	"bufio"
+	"hmcalister/aoc13/lib"
+
+	"github.com/rs/zerolog/log"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	return 0, nil
+	filePatterns := lib.ParseFileToPatterns(fileScanner)
+	log.Debug().Int("NumberOfPatterns", len(filePatterns)).Send()
+
+	result := 0
+	for _, pattern := range filePatterns {
+		log.Debug().Interface("Pattern", pattern).Send()
+		rowReflectionIndex, columnReflectionIndex := pattern.FindSmudgedReflectionsIndices()
+
+		result += rowReflectionIndex + 100*columnReflectionIndex
+	}
+
+	return result, nil
 }
