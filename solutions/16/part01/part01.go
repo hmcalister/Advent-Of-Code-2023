a0878f8918b5680c854ab79af689a84d7c01af4a
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-16 21:15:57 +1300

    Update part01 to conform to new lib interface

--- solutions/16/part01/part01.go
+++ solutions/16/part01/part01.go
@@ -3,20 +3,20 @@ package part01
 import (
 	"bufio"
 	"hmcalister/aoc16/lib"
-	"slices"
 
 	"github.com/rs/zerolog/log"
 )
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	layout := lib.NewLayoutData(fileScanner)
+	layout := lib.NewLayoutData(fileScanner, &lib.LightRay{
+		Direction: lib.DIRECTION_EAST,
+		XCoord:    0,
+		YCoord:    0,
+	})
 	layout.ShowLayout()
 	layout.ProcessLayout()
-	layout.ShowEnergizedCells()
-
-	slices.Sort(layout.EnergizedLinearCoordinates)
-	layout.EnergizedLinearCoordinates = slices.Compact(layout.EnergizedLinearCoordinates)
 	log.Debug().Interface("EnergizedLinearCoords", layout.EnergizedLinearCoordinates).Send()
+	layout.ShowEnergizedCells()
 
 	return len(layout.EnergizedLinearCoordinates), nil
 }
