4a387f95cccb230027ea66c712dd473027500321
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-17 18:59:15 +1300

    Add part01 with lib impl

--- solutions/17/part01/part01.go
+++ solutions/17/part01/part01.go
@@ -1,7 +1,18 @@
 package part01
 
-import "bufio"
+import (
+	"bufio"
+	"hmcalister/aoc17/lib"
+
+	"github.com/rs/zerolog/log"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	return 0, nil
+	costMap := lib.GetCostMapFromFileScanner(fileScanner)
+	layout := lib.NewLayout(costMap)
+	goalState := layout.AStarPathFind()
+	if goalState == nil {
+		log.Fatal().Msg("no path found")
+	}
+	return goalState.GScore, nil
 }
