a2e43339d1ea13c2c91b2876e3272f46f54130d9
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-18 01:23:03 +1300

    Add part01 to conform to new lib impl

--- solutions/17/part01/part01.go
+++ solutions/17/part01/part01.go
@@ -3,15 +3,10 @@ package part01
 import (
 	"bufio"
 	"hmcalister/aoc17/lib"
-
-	"github.com/rs/zerolog/log"
 )
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	layout := lib.NewLayout(fileScanner)
-	goalState := layout.DijkstraPathFind()
-	if goalState == nil {
-		log.Fatal().Msg("no path found")
-	}
-	return goalState.TraversedDistance, nil
+	layout := lib.NewLayoutFromFileScanner(fileScanner, 0, 3)
+	goalDist := layout.PathFind()
+	return goalDist, nil
 }
