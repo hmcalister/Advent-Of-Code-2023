0a80c2bc1fab93902bd01f96da396d08297c7e2b
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-18 01:16:34 +1300

    Update path finding node impl

--- solutions/17/lib/pathFindNode.go
+++ solutions/17/lib/pathFindNode.go
@@ -2,55 +2,42 @@ package lib
 
 import (
 	"fmt"
-
-	"github.com/rs/zerolog/log"
 )
 
-type coordinate struct {
-	X int
-	Y int
-}
-
-func (c coordinate) String() string {
-	return fmt.Sprintf("(%v, %v)", c.X, c.Y)
-}
-
-func (c coordinate) Move(direction DirectionEnum) coordinate {
-	switch direction {
-	case DIRECTION_NORTH:
-		c.Y -= 1
-	case DIRECTION_EAST:
-		c.X += 1
-	case DIRECTION_SOUTH:
-		c.Y += 1
-	case DIRECTION_WEST:
-		c.X -= 1
-	case DIRECTION_UNDEFINED:
-		log.Fatal().Msg("cannot add undefined direction to coordinate")
-	}
-
-	return c
-}
-
 type pathFindNodeData struct {
-	Coordinate                coordinate
-	Direction                 DirectionEnum
-	DirectionConsecutiveCount int
-	Distance                  int
-	HashString                string
+	Coordinate coordinate
+	Direction  DirectionEnum
+	Streak     int
+	Cost       int
 }
 
-func newPathFindNode(c coordinate, direction DirectionEnum, directionConsecutiveCount int, distance int) *pathFindNodeData {
-	return &pathFindNodeData{
-		Coordinate:                c,
-		Direction:                 direction,
-		DirectionConsecutiveCount: directionConsecutiveCount,
-		Distance:                  distance,
-		// HashString:                c.String(),
-		HashString: fmt.Sprintf("%v %v %v", c.String(), direction.String(), directionConsecutiveCount),
+func newPathFindNode(c coordinate, direction DirectionEnum, streak int, cost int) pathFindNodeData {
+	return pathFindNodeData{
+		Coordinate: c,
+		Direction:  direction,
+		Streak:     streak,
+		Cost:       cost,
 	}
 }
 
-func (node *pathFindNodeData) String() string {
-	return fmt.Sprintf("%v %v (%v-%v)", node.Coordinate.String(), node.Distance, node.Direction.String(), node.DirectionConsecutiveCount)
+func (node pathFindNodeData) String() string {
+	return fmt.Sprintf("%v COST: %v (%v STREAK %v)", node.Coordinate.String(), node.Cost, node.Direction.String(), node.Streak)
+}
+
+func (node pathFindNodeData) Hash() string {
+	return fmt.Sprintf("%v %v %v", node.Coordinate.String(), node.Direction.String(), node.Streak)
+}
+
+type PriorityQueue []pathFindNodeData
+
+func (pq PriorityQueue) Len() int            { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool  { return pq[i].Cost < pq[j].Cost }
+func (pq PriorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(pathFindNodeData)) }
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
 }
