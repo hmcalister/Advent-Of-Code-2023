074998b95b7207abf14bf2bf3414ea17ca48bb6b
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-19 18:53:37 +1300

    Add part01 implementation

--- solutions/19/part01/part01.go
+++ solutions/19/part01/part01.go
@@ -1,7 +1,47 @@
 package part01
 
-import "bufio"
+import (
+	"bufio"
+	"hmcalister/aoc19/lib"
+
+	"github.com/rs/zerolog/log"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	return 0, nil
+	var line string
+
+	workflowMap := make(map[string]lib.Workflow)
+
+	// Parse the workflows
+	for fileScanner.Scan() {
+		line = fileScanner.Text()
+		if len(line) == 0 {
+			break
+		}
+		log.Debug().
+			Str("RawLine", line).
+			Send()
+
+		newWorkflow := lib.ParseLineToWorkflow(line)
+		workflowMap[newWorkflow.WorkflowName] = newWorkflow
+
+	}
+
+	totalAcceptedRatings := 0
+	// Parse the parts
+	for fileScanner.Scan() {
+		line = fileScanner.Text()
+		part := lib.ParseLineToPartData(line)
+
+		log.Debug().
+			Str("RawLine", line).
+			Interface("ParsedPart", part).
+			Send()
+
+		if lib.ProcessPart(part, workflowMap) {
+			totalAcceptedRatings += part.SumRatings()
+		}
+	}
+
+	return totalAcceptedRatings, nil
 }
