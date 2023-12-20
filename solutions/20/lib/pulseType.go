d47b5558014cb3ba1e4b0a35125097acad25d5ca
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-20 18:57:33 +1300

    Add pulse type to lib

---
+++ solutions/20/lib/pulseType.go
@@ -0,0 +1,10 @@
+package lib
+
+//go:generate stringer -type PulseTypeEnum
+type PulseTypeEnum int
+
+const (
+	NO_PULSE   PulseTypeEnum = iota
+	LOW_PULSE  PulseTypeEnum = iota
+	HIGH_PULSE PulseTypeEnum = iota
+)
