3eda3194c497fa69a47b58756aefee5bdcc8521a
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-20 18:58:05 +1300

    Update part01 with implementation of solution

--- solutions/20/part01/part01.go
+++ solutions/20/part01/part01.go
@@ -1,7 +1,28 @@
 package part01
 
-import "bufio"
+import (
+	"bufio"
+	"hmcalister/aoc20/lib"
+
+	"github.com/rs/zerolog/log"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	return 0, nil
+	moduleConfig := lib.ParseFileToModuleConfiguration(fileScanner)
+
+	for i := 0; i < 1000; i += 1 {
+		moduleConfig.PushButton()
+	}
+
+	lowPulses := moduleConfig.TotalPulses[lib.LOW_PULSE]
+	highPulses := moduleConfig.TotalPulses[lib.HIGH_PULSE]
+	result := lowPulses * highPulses
+	log.Debug().
+		Int("LowPulseCount", lowPulses).
+		Int("HighPulseCount", highPulses).
+		Int("TotalPulses", lowPulses+highPulses).
+		Int("Result", result).
+		Send()
+
+	return result, nil
 }
