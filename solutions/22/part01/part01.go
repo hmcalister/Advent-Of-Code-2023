7b29e403e23afa7cca109319fad107911cc0a31d
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-23 00:13:34 +1300

    Update main.go with logic to calculate free bricks

--- solutions/22/part01/part01.go
+++ solutions/22/part01/part01.go
@@ -3,10 +3,36 @@ package part01
 import (
 	"bufio"
 	"hmcalister/aox22/lib"
+
+	"github.com/rs/zerolog/log"
 )
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
-	lib.ParseFileToBrickPile(fileScanner)
+	pile := lib.ParseFileToBrickPile(fileScanner)
+	pile.SimulateBrickFall()
 
-	return 0, nil
+	disintegrateBrickIndicesMap := make(map[int]bool)
+	for i := 0; i < len(pile.Bricks); i += 1 {
+		disintegrateBrickIndicesMap[i] = true
+	}
+	for i := 0; i < len(pile.Bricks); i += 1 {
+		log.Info().
+			Int("BrickIndex", i).
+			Str("Brick", pile.Bricks[i].String()).
+			Interface("SupportingBricks", pile.Supports[i]).
+			Send()
+		if len(pile.Supports[i]) == 1 {
+			disintegrateBrickIndicesMap[pile.Supports[i][0]] = false
+		}
+	}
+
+	disintegrateBrickIndicesArr := make([]int, 0)
+	for i := 0; i < len(pile.Bricks); i += 1 {
+		if disintegrateBrickIndicesMap[i] {
+			disintegrateBrickIndicesArr = append(disintegrateBrickIndicesArr, i)
+		}
+	}
+	log.Info().Interface("DisintegrateBrickIndices", disintegrateBrickIndicesArr).Send()
+
+	return len(disintegrateBrickIndicesArr), nil
 }
