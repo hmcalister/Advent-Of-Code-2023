2887690380a3ecc61643e1dad1a0f9b1faf93f35
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-22 23:01:08 +1300

    Add brick impl for lib

---
+++ solutions/22/lib/brick.go
@@ -0,0 +1,71 @@
+package lib
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+type BrickData struct {
+	ID      int
+	Start   Coordinate
+	Lengths Coordinate
+}
+
+func ParseLineToBrick(line string) BrickData {
+	lineFields := strings.FieldsFunc(line, func(r rune) bool {
+		return r == '~'
+	})
+	brickStarts := StringToCoordinate(lineFields[0])
+	brickEnds := StringToCoordinate(lineFields[1])
+	parsedBrick := createBrickFromCoordinates(brickStarts, brickEnds)
+
+	log.Debug().
+		Str("RawLine", line).
+		Interface("StringFields", lineFields).
+		Str("ParsedBrick", parsedBrick.String()).
+		Send()
+
+	return parsedBrick
+}
+
+func createBrickFromCoordinates(brickStart, brickEnd Coordinate) BrickData {
+	brickLens := Coordinate{
+		X: brickEnd.X - brickStart.X + 1,
+		Y: brickEnd.Y - brickStart.Y + 1,
+		Z: brickEnd.Z - brickStart.Z + 1,
+	}
+
+	if brickLens.X < 0 || brickLens.Y < 0 || brickLens.Z < 0 {
+		log.Fatal().Interface("BrickLens", brickLens).Msg("found negative brick len")
+	}
+
+	return BrickData{
+		Start:   brickStart,
+		Lengths: brickLens,
+	}
+}
+
+func (brick BrickData) enumerateCoordinates() []Coordinate {
+	volume := brick.Lengths.X * brick.Lengths.Y * brick.Lengths.Z
+	allCoordinates := make([]Coordinate, volume)
+	for dx := 0; dx < brick.Lengths.X; dx += 1 {
+		for dy := 0; dy < brick.Lengths.Y; dy += 1 {
+			for dz := 0; dz < brick.Lengths.Z; dz += 1 {
+				allCoordinates = append(allCoordinates, Coordinate{
+					X: brick.Start.X + dx,
+					Y: brick.Start.Y + dy,
+					Z: brick.Start.Z + dz,
+				})
+			}
+		}
+	}
+
+	return allCoordinates
+}
+
+func (brick BrickData) String() string {
+	volume := brick.Lengths.X * brick.Lengths.Y * brick.Lengths.Z
+	return fmt.Sprintf("%v, %v (Vol: %v)", brick.Start.String(), brick.Lengths.String(), volume)
+}
