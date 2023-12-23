5a6ab3c3f30ea6a5534799b34979bd7cfa960430
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-23 18:16:29 +1300

    Add surface type enum for lib

---
+++ solutions/23/lib/surfaceType.go
@@ -0,0 +1,23 @@
+package lib
+
+type SurfaceTypeEnum int
+
+const (
+	SURFACE_FOREST      SurfaceTypeEnum = iota
+	SURFACE_PATH        SurfaceTypeEnum = iota
+	SURFACE_SLOPE_UP    SurfaceTypeEnum = iota
+	SURFACE_SLOPE_RIGHT SurfaceTypeEnum = iota
+	SURFACE_SLOPE_DOWN  SurfaceTypeEnum = iota
+	SURFACE_SLOPE_LEFT  SurfaceTypeEnum = iota
+)
+
+var (
+	runeToSurfaceTypeMap map[rune]SurfaceTypeEnum = map[rune]SurfaceTypeEnum{
+		'#': SURFACE_FOREST,
+		'.': SURFACE_PATH,
+		'^': SURFACE_SLOPE_UP,
+		'>': SURFACE_SLOPE_RIGHT,
+		'v': SURFACE_SLOPE_DOWN,
+		'<': SURFACE_SLOPE_LEFT,
+	}
+)
