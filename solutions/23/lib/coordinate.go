e118e5f4a5d2c1bc53393d94c218f87aa3bd5252
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-23 22:18:00 +1300

    Update coord with Move method

--- solutions/23/lib/coordinate.go
+++ solutions/23/lib/coordinate.go
@@ -10,3 +10,19 @@ type Coordinate struct {
 func (coord Coordinate) String() string {
 	return fmt.Sprintf("(%v, %v)", coord.X, coord.Y)
 }
+
+func (coord Coordinate) Move(direction DirectionEnum) Coordinate {
+	newCoord := coord
+	switch direction {
+	case DIRECTION_UP:
+		newCoord.Y -= 1
+	case DIRECTION_RIGHT:
+		newCoord.X += 1
+	case DIRECTION_DOWN:
+		newCoord.Y += 1
+	case DIRECTION_LEFT:
+		newCoord.X -= 1
+	}
+
+	return newCoord
+}
