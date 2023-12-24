89b92c6d3b24764e37e1def71a9c8b5c71d79ce8
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-24 21:17:59 +1300

    Update part02 with defeat!

--- solutions/24/part02/part02.go
+++ solutions/24/part02/part02.go
@@ -1,7 +1,13 @@
 package part02
 
-import "bufio"
+import (
+	"bufio"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
+	// Part02 requires some rather tricky solutions / optimizations. It is much easier to do this with Sympy (see part02.py).
+	//
+	// One day I will figure out how to do this in Go!
+
 	return 0, nil
 }
