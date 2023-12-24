0749f597a0ad65cdbdd10b4ba28cd29d460ec805
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-24 17:30:57 +1300

    Add main.go for day24

---
+++ solutions/24/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"hmcalister/aoc24/part01"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+const INPUT_FILE_PATH = "puzzleInput"
+
+func init() {
+	logToFileFlag := flag.Bool("logToFile", false, "Flag to log to file, rather than to console output")
+	flag.Parse()
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	if *logToFileFlag {
+		logFile, err := os.Create("log")
+		if err != nil {
+			log.Fatal().Msgf("Count not open log file: %v", err)
+		}
+		log.Logger = zerolog.New(logFile).With().Timestamp().Logger()
+	}
+}
+
+func main() {
+	file, err := os.Open(INPUT_FILE_PATH)
+	if err != nil {
+		log.Panic().Msgf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	result, err := part01.ProcessInput(fileScanner)
+	if err != nil {
+		log.Panic().Msgf("error processing file input: %v", err)
+	}
+
+	log.Info().
+		Int("Result", result).
+		Send()
+}
