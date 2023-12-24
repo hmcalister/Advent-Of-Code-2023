11aeeb5ae3cce06b1845a414d03b43ae08f96880
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-24 19:03:29 +1300

    Update part01 with testing

--- solutions/24/part01/part01.go
+++ solutions/24/part01/part01.go
@@ -1,7 +1,13 @@
 package part01
 
-import "bufio"
+import (
+	"bufio"
+	"hmcalister/aoc24/lib"
+)
 
 func ProcessInput(fileScanner *bufio.Scanner) (int, error) {
+	storm := lib.ParseFileToStorm(fileScanner)
+	storm.DetectCollisionsInXY(0, 1)
+
 	return 0, nil
 }
